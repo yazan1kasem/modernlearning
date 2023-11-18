40a4d22343f938fefa2e729c2004348101cc95d8
yazan204 <118280703+yazan204@users.noreply.github.com>
2023-11-18 16:08:18 +0100

    version 1.1

--- src/main/java/modern/learning/modernlearning/Starter.java
+++ src/main/java/modern/learning/modernlearning/Starter.java
@@ -3,6 +3,7 @@ package modern.learning.modernlearning;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import javax.persistence.EntityManager;
@@ -12,13 +13,13 @@ import java.io.IOException;
 public class Starter extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        EntityManager emf= Persistence.createEntityManagerFactory("Modernlearning").createEntityManager();
 
-//        FXMLLoader fxmlLoader = new FXMLLoader(Starter.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-//        stage.setTitle("Hello!");
-//        stage.setScene(scene);
-//        stage.show();
+        FXMLLoader fxmlLoader = new FXMLLoader(Starter.class.getResource("Arbeitsblätter.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Modern learning");
+        stage.getIcons().add(new Image("file:src/main/Media/SkillBuildersLogo.png"));
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
