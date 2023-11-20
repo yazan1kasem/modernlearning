7ed7a1b79abaf2255dd1c7cd7f4ec564f67e6132
yazan204 <118280703+yazan204@users.noreply.github.com>
2023-11-20 14:46:36 +0100

    version 1.4

--- src/main/java/modern/learning/modernlearning/Kalender.java
+++ src/main/java/modern/learning/modernlearning/Kalender.java
@@ -1,4 +1,13 @@
 package modern.learning.modernlearning;
 
-public class Kalender {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Kalender implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
--- src/main/java/modern/learning/modernlearning/Starter.java
+++ src/main/java/modern/learning/modernlearning/Starter.java
@@ -3,23 +3,31 @@ package modern.learning.modernlearning;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class Starter extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(Starter.class.getResource("Arbeitsblätter.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Starter.class.getResource("Kalender.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Modern learning");
         stage.getIcons().add(new Image("file:src/main/Media/SkillBuildersLogo.png"));
+        stage.setWidth(scene.getWidth());
+
+        stage.setScene(scene);
 
         stage.show();
+
     }
 
     public static void main(String[] args) {
