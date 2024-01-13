89b79fe282cdd9ce58e7fb241586c20e8eced443
yazan204 <118280703+yazan204@users.noreply.github.com>
2024-01-13 14:15:30 +0100

    version 1.26

--- src/main/java/modern/learning/modernlearning/Klasse.java
+++ src/main/java/modern/learning/modernlearning/Klasse.java
@@ -13,27 +13,33 @@ import java.awt.*;
 
 public class Klasse {
     @FXML
-    public void handleClassSelection(MouseEvent event) {
-        Stage currentStage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        try {
-            Node source = (Node) event.getSource();
-            String klassenid = source.getId();
-            FXMLLoader fxmlLoader = new FXMLLoader(Starter.class.getResource("Fach.fxml"));
-
-            Fach fach = new Fach(klassenid);
-            fxmlLoader.setController(fach);
-            Scene scene = new Scene(fxmlLoader.load(),currentStage.getWidth(),currentStage.getHeight());
-            currentStage.setScene(scene);
-        }catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+    public void handleClassSelection(ActionEvent event) {
+        // Hier kommt der Code, den Sie ausführen möchten,
+        // wenn auf einen Klassen-Button geklickt wird.
+        // Sie können beispielsweise die ausgewählte Klasse erhalten
+        // und entsprechende Aktionen durchführen.
+        // Zum Beispiel:
+        // Object source = event.getSource();
+        // if (source instanceof Button) {
+        //    Button clickedButton = (Button) source;
+        //    String selectedClass = clickedButton.getText();
+        //    // Führen Sie hier Aktionen für die ausgewählte Klasse durch.
+        // }
     }
     public void zurück(MouseEvent mouseEvent) {
         Stage currentStage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+        currentStage.close();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Klasse.fxml"));
         try {
 
             FXMLLoader fxmlLoader = new FXMLLoader(Starter.class.getResource("Kalender.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(),currentStage.getWidth(),currentStage.getHeight());
+            Scene scene = new Scene(fxmlLoader.load());
+            currentStage.setTitle("Modern learning");
+            currentStage.getIcons().add(new Image("file:src/main/Media/SkillBuildersLogo.png"));
+            currentStage.setMinHeight(640);
+            currentStage.setMinWidth(1000);
+            currentStage.setWidth(scene.getWidth());
+
             currentStage.setScene(scene);
 
             currentStage.show();
