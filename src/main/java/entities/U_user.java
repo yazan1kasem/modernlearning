6afd3e2c7178964736586a2903ce01d398fbccaf
SINO420 <sino.akrout@gmail.com>
2024-04-20 13:32:14 +0200

    version 2.02

--- src/main/java/entities/U_user.java
+++ src/main/java/entities/U_user.java
@@ -10,20 +10,9 @@ public class U_user {
 
     @Id
     @Column(name = "U_id", unique = true, nullable = false)
-    private String U_id;
+    private int U_id;
 
     @Column(name="U_Name")
     private String U_Name;
-
-    @Column(name="U_Passwort")
-    private char U_Passwort;
-
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name="U_E_id", referencedColumnName = "E_id")
-    private List<E_EigeneArbeitsblätter> e_eigeneArbeitsblätters=new ArrayList<E_EigeneArbeitsblätter>();
-
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name="U_K_id", referencedColumnName = "K_ID")
-    private List<K_Kalender> k_kalenders=new ArrayList<K_Kalender>();
 
 }
