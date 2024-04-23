1dd99d15e3979d82ab4b2d69b5b86396de0f4b6c
yazan204 <118280703+yazan204@users.noreply.github.com>
2024-04-23 12:13:36 +0200

    version 2.13

---
+++ src/main/java/modern/learning/modernlearning/Curentuser.java
@@ -0,0 +1,8 @@
+package modern.learning.modernlearning;
+
+public abstract class Curentuser {
+    static private String user;
+     public static void setUsername(String usernamen){
+        user = usernamen;
+    }
+}
